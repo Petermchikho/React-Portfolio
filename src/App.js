ed723611ebad114443a5cdb02d06a427455b2559
peter mchikho <petercharlesmchikho1@gmail.com>
2024-03-09 12:49:17 +0200

    1.20

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import About from './pages/About';
+import Vans from './pages/Vans';
+import VanDetails from './pages/Vandetails';
 import {BrowserRouter,Routes,Route,Link} from 'react-router-dom';
-
+import "./server"
 function App() {
   return (
     <BrowserRouter>
@@ -12,11 +14,14 @@ function App() {
         <Link className="site-logo" to="/">#VanLife</Link>
         <nav>
           <Link to="/about">About</Link>
+          <Link to="/vans">Vans</Link>
         </nav>
       </header>
      <Routes>
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
+      <Route path='/vans' element={<Vans />}/>
+      <Route path='/vans/:id' element={<VanDetails />}/>
      </Routes>
   </BrowserRouter>
   );
